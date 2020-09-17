43674fa34ed9d74818d6a6c704a36902230b880f
tim.s <shenyang@shenyangs-MacBook-Pro.local>
2020-09-17 14:47:32 +0800

    address

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
   <div id="app">
     <ul>
       <li v-for="(item, key) in this.adderssList" :key="key">
-        <span>{{ item }}</span>
+        <p>{{ item }}</p>
+        <p>初始值：{{ old_wallet[item] }}</p>
+        <span>当前值：{{ wallet[item] }}</span>
       </li>
     </ul>
   </div>
@@ -15,23 +17,17 @@ export default {
   data() {
     return {
       adderssList: [
-        "TZ7LFhgZtHdPgfetaQioB6RGPHv44K5YSV",
-        "TQgAJin7TSHRxMYC67CJ713ct7DQ7hbPRE",
-        "TFLQwhip363fMkBATfDPTUYibzy9GmsZYJ",
-        "TVgam8pQNJ5sXuwRQye2MuGB4p5Q1MFQvw",
-        "TH2SfRrGg1XRuWvQJebvfd9cGdoqARybBF",
-        "TXZ4eQ3RQHHDeVNhCCXLEJP5WkynVU9d4T",
-        "TQipTdM4cqxDQv7rRoKmGwjfuNdb3mm6Pe",
-        "TYZAxWS1RvhL5gamUgDnqvfmKHaa6kZKGh",
-        "TX4mNg9Sfmr3gxaKi9tB9yP6CxXXTmXSL2",
-        "TRvAGZSngkLokUh3FndPCFaXsw1sGfrHSo",
-        "TVPawUgZpK18eme2Z9D3YLpJ5ELSdq6URm",
-        "TJuGTgb1x9o4Fwq9kTr7Tw1NKtzk74qvaC",
-        "TUE2GfHt5ZrywFwqNdeY7HtLNQ2nXQLzqe",
-        "TAqvdh7hMCXX7sMCqLpkG9EPdqapuWHf2w",
-        "TTY1TDe5tNaCLn2wEARP1AGY3EnZwt5Qak",
-        "TEN3gquz1RwsMfPnKVEzDWQi9MWDn1Pt1C"
-      ]
+        "TNawcJhC2GrrrrhxdWXPTD8Dcr2848m3KF",
+        "TFLvfXzFtDsvPKnS2fc4zv28kowzgEwjh2"
+      ],
+      old_wallet: {
+        TNawcJhC2GrrrrhxdWXPTD8Dcr2848m3KF: 124734,
+        TFLvfXzFtDsvPKnS2fc4zv28kowzgEwjh2: 80255
+      },
+      wallet: {
+        TNawcJhC2GrrrrhxdWXPTD8Dcr2848m3KF: 124734,
+        TFLvfXzFtDsvPKnS2fc4zv28kowzgEwjh2: 80255
+      }
     };
   },
   mounted() {
@@ -39,27 +35,26 @@ export default {
     setInterval(() => {
       axios
         .get(
-          `https://apilist.tronscan.org/api/account?address=TL6Fpt8pEdTYPN8GNa1phAAFKdNjrnkFuS`
+          `https://apilist.tronscan.org/api/account?address=${this.adderssList[k]}`
         )
         .then(res => {
-          console.log(res.data.trc20token_balances);
           let balanceList = res.data.trc20token_balances;
           if (balanceList.length > 0) {
-            console.log(balanceList);
             balanceList.forEach(token => {
-              if (token.symbol === "CC") {
-                console.log(
-                  token.balance.substring(
-                    0,
-                    token.balance.length - token.decimals
-                  )
+              if (
+                token.symbol === "IBT" &&
+                token.contract_address === "TWSuK6c6h9NrnXZEHLrnu8DHaDv1kNFgf6"
+              ) {
+                this.wallet[res.data.address] = token.balance.substring(
+                  0,
+                  token.balance.length - token.decimals
                 );
               }
             });
           }
         });
       if (k === this.adderssList.length - 1) {
-        k = 0;
+        k = -1;
       }
       k++;
     }, 2000);
@@ -78,5 +73,9 @@ export default {
 }
 li {
   list-style: none;
+  height: 156px;
+  box-shadow: rgba(0, 0, 0, 0.5) 0px 0px 3rem -1rem;
+  padding-top: 50px;
+  margin-bottom: 20px;
 }
 </style>
